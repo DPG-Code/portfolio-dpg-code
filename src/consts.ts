08b826066790e2f932d428023b75a1018d05b086
Daniel Prieto <prietogaviriadaniel@gmail.com>
2025-11-30 11:27:51 +0100

    feat: new components styles and new projects images

--- src/consts.ts
+++ src/consts.ts
@@ -9,43 +9,49 @@ const mainProject = {
 
 const moreWorks = [
   {
-    title: 'JobsTracker',
-    description: 'Infojobs hackathon finalist, a tool to gather job position information effectively.',
-    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412375/works/moreworks/bento/png/jobstracker_l47mv0.webp',
-    url: 'https://infojobs-tracker.vercel.app/'
-  },
-  {
     title: 'ShoppyFree',
     description:
-      'Next.js-based e-commerce platform packed with feature-rich functionalities.',
-    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412374/works/moreworks/bento/png/shoppyfree_oj6vmi.webp',
-    url: 'https://shoppyfree.vercel.app/'
-  },
-  {
-    title: 'WriteRight',
-    description: 'English text correction app powered by AI.',
-    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412375/works/moreworks/bento/png/writeright_k8owmh.webp',
-    url: 'https://github.com/DPG-Code/write-right'
+      'Ecommerce website.',
+    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412374/works/moreworks/bento/png/shoppyfree_qlmmkt',
+    url: 'https://shoppyfree.vercel.app/',
+    year: '2025'
   },
   {
     title: 'Gifree',
     description:
-      'Enjoy a user-friendly GIF platform with user profiles and favoriting features.',
-    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412380/works/moreworks/bento/png/gifree_huz8df.webp',
-    url: 'https://gifree.vercel.app/'
+      'GIF platform.',
+    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412380/works/moreworks/bento/png/gifree_t4qys6',
+    url: 'https://gifree.vercel.app/',
+    year: '2024'
   },
   {
-    title: 'Cleanbg',
-    description: 'Quickly remove image backgrounds with our app.',
-    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412374/works/moreworks/bento/png/cleanbg_gfbtp0.webp',
-    url: 'https://clean-bg.vercel.app/'
+    title: 'JobsTracker',
+    description: 'Infojobs hackathon.',
+    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412375/works/moreworks/bento/png/jobstracker_vfhvzp',
+    url: 'https://infojobs-tracker.vercel.app/',
+    year: '2023'
   },
   {
     title: 'Notes app',
     description:
-      'User-friendly note-taking web app powered by MongoDB for data storage.',
-    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412375/works/moreworks/bento/png/notesapp_p5cdji.webp',
-    url: 'https://github.com/DPG-Code/notes-app'
+      'Notes web app.',
+    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412375/works/moreworks/bento/png/notesapp_b2fjbg',
+    url: 'https://github.com/DPG-Code/notes-app',
+    year: '2023'
+  },
+  {
+    title: 'Cleanbg',
+    description: 'Background remove.',
+    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412374/works/moreworks/bento/png/cleanbg_lih1x8',
+    url: 'https://clean-bg.vercel.app/',
+    year: '2023'
+  },
+  {
+    title: 'WriteRight',
+    description: 'Text correction by AI.',
+    img: 'https://res.cloudinary.com/dhpxqwsym/image/upload/v1764412375/works/moreworks/bento/png/writeright_lb8wjm',
+    url: 'https://github.com/DPG-Code/write-right',
+    year: '2023'
   }
 ]
 
